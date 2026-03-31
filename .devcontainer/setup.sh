ed36b00a6da13e4bfa70e875c7c4b23ec12ab03c
Michelangelo <ms.co.it@gmail.com>
2026-03-31 15:12:29 +0000

    minors

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -6,6 +6,7 @@ curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --yes --de
 # echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg && apt-get update -y && apt-get install google-cloud-cli -y
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 sudo apt-get -y update && sudo apt-get install google-cloud-cli
+cargo install --git https://github.com/0xMassi/webclaw.git webclaw-cli
 # npm i -D wrangler@latest
 # rustup target add wasm32-unknown-unknown
 # cargo install cargo-generate
