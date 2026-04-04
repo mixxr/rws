d9214602d5bad329e8cbcca70db16ac361c1499d
Michelangelo <ms.co.it@gmail.com>
2026-04-04 13:18:42 +0200

    jobmonitor dockerignore

---
+++ crates/jobmonitor/build.sh
@@ -0,0 +1,4 @@
+gcloud beta run jobs deploy jobmonitor --source=. --execute-now --region=europe-west1 \
+--add-volume name=gcs-1,bucket=rws-data,type=cloud-storage \
+--add-volume-mount volume=gcs-1,mount-path=/data \
+--set-env-vars BUCKET_NAME=rws-data
