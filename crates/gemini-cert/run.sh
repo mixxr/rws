2af7543bf11d055782741d173a2fd31c66ea8ceb
Michelangelo <ms.co.it@gmail.com>
2026-04-13 17:09:30 +0000

    gemini-cert standalone

--- crates/gemini-cert/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -a
-source .env.local
-cargo run -- -i /tmp/data/certificates/input/DE000VH68M19.md -o /tmp/data/certificates/output -l config/models.csv -n DE000VH68M19 -p 1
-set +a
