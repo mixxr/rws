81cbf7d82f167cd8e732c510358e8468c76b465a
Michelangelo <ms.co.it@gmail.com>
2026-04-13 15:13:38 +0000

    gemini-cert cmd example

--- crates/gemini-cert/run.sh
+++ crates/gemini-cert/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -a
 source .env.local
-cargo run -- -i config/bnp-1.csv -o ./data/bnp -p 5
+cargo run -- -i /tmp/data/certificates/input/DE000VH68M19.md -o /tmp/data/certificates/output -l config/models.csv -n DE000VH68M19 -p 1
 set +a
