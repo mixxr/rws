651b0abbcbcd24ba4cbd7c3a7c26d27fb04cbf8d
mixxr <ms.co.it@gmail.com>
2026-04-14 16:59:08 +0200

    app certs bq

--- apps/certificates/bq/docker/setup.sh
+++ apps/certificates/bq/docker/setup.sh
@@ -4,8 +4,6 @@ TYPE="certificates"
 STATUS="bq"
 mntdir=${MOUNT_DIR:-.}/${TYPE}
 echo $mntdir
-echo "#!/bin/bash" > ./run.sh
-echo "ERROR_COUNT=0" >> ./run.sh
 echo "Reading manifest file from $mntdir/jobs/$STATUS/*.txt"
 manifest_file=$(ls "$mntdir/jobs/$STATUS"/*.txt 2>/dev/null | head -n 1)
 if [ -z "$manifest_file" ]; then
@@ -14,20 +12,21 @@ if [ -z "$manifest_file" ]; then
 fi
 echo "Processing manifest file $manifest_file"
 log_file="$manifest_file".log
-while IFS= read -r line || [[ -n "$line" ]]; do
-    line=$(echo "$line" | tr -d '\r' | xargs)
-    echo "bq load \\
-      --autodetect=false --source_format=NEWLINE_DELIMITED_JSON \\
-      --schema_update_option=ALLOW_FIELD_ADDITION \\
-      --file_set_spec_type=NEW_LINE_DELIMITED_MANIFEST \\
-      ISINs.isin_ticker \\
-      $line 2>> $log_file" >> ./run.sh
-    echo "if [ \$? -ne 0 ]; then 
-      echo 'ERROR loading $line' 
-      ((ERROR_COUNT++))
-    fi" >> ./run.sh
-done < "$manifest_file"
-echo "echo 'ERROR COUNTER: \$ERROR_COUNT'
+
+echo "#!/bin/bash" > ./run.sh
+echo "ERROR_COUNT=0" >> ./run.sh
+echo "bq load \\
+  --autodetect=false --source_format=NEWLINE_DELIMITED_JSON \\
+  --schema_update_option=ALLOW_FIELD_ADDITION \\
+  --file_set_spec_type=NEW_LINE_DELIMITED_MANIFEST \\
+  ISINs.isin_ticker \\
+  $manifest_file 2>> $log_file" >> ./run.sh
+echo "if [ \$? -ne 0 ]; then 
+  echo 'ERROR loading $manifest_file' 
+  ((ERROR_COUNT++))
+fi" >> ./run.sh
+
+echo "echo ERROR COUNTER: \$ERROR_COUNT
 if [ \$ERROR_COUNT -gt 0 ]; then
   mv $manifest_file $manifest_file.error
   exit 2
@@ -35,10 +34,6 @@ else
   mv $manifest_file $manifest_file.done
 fi" >> ./run.sh
 
-if ! test -f ./run.sh; then
-  echo "No execution to build."
-  exit 1
-fi
 chmod +x ./run.sh
 echo -e "Setup completed:\n $(cat ./run.sh)"
 ./run.sh
