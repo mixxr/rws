8a57762e6fda71cb80f40cf850c39ff7dcc54448
mixxr <ms.co.it@gmail.com>
2026-04-14 17:41:12 +0200

    loop in bq app

--- apps/certificates/bq/docker/setup.sh
+++ apps/certificates/bq/docker/setup.sh
@@ -5,35 +5,32 @@ STATUS="bq"
 mntdir=${MOUNT_DIR:-.}/${TYPE}
 bucket=${MOUNT_BUCKET}/${TYPE}
 echo "Mount Dir: $mntdir -> $bucket"
-echo "Reading manifest file from $mntdir/jobs/$STATUS/*.txt"
-manifest_file=$(ls "$mntdir/jobs/$STATUS"/*.txt 2>/dev/null | head -n 1)
-if [ -z "$manifest_file" ]; then
-  echo "No manifest file found. Exiting."
-  exit 1
-fi
-echo "Processing manifest file $manifest_file"
-log_file="$manifest_file".log
-datetime=$(basename "$manifest_file" .txt)
+echo "Reading manifest files from $mntdir/jobs/$STATUS/*.txt"
 
 echo "#!/bin/bash" > ./run.sh
 echo "ERROR_COUNT=0" >> ./run.sh
-echo "bq load \\
-  --autodetect=false --source_format=NEWLINE_DELIMITED_JSON \\
-  --schema_update_option=ALLOW_FIELD_ADDITION \\
-  --file_set_spec_type=NEW_LINE_DELIMITED_MANIFEST \\
-  ISINs.isin_ticker \\
-  $bucket/jobs/$STATUS/$datetime.txt 2>> $log_file" >> ./run.sh
-echo "if [ \$? -ne 0 ]; then 
-  echo 'ERROR loading $manifest_file' 
-  ((ERROR_COUNT++))
-fi" >> ./run.sh
+for manifest_file in "$mntdir/jobs/$STATUS"/*.uri; do
+  echo "Processing manifest file $manifest_file"
+  log_file="$manifest_file".log
+  datetime=$(basename "$manifest_file" .txt)
+  echo "bq load \\
+    --autodetect=false --source_format=NEWLINE_DELIMITED_JSON \\
+    --schema_update_option=ALLOW_FIELD_ADDITION \\
+    --file_set_spec_type=NEW_LINE_DELIMITED_MANIFEST \\
+    ISINs.isin_ticker \\
+    $bucket/jobs/$STATUS/$datetime.txt 2>> $log_file" >> ./run.sh
+  echo "if [ \$? -eq 0 ]; then
+    mv $manifest_file $manifest_file.done
+  else 
+    echo 'ERROR loading $manifest_file' 
+    mv $manifest_file $manifest_file.error
+    ((ERROR_COUNT++))
+  fi" >> ./run.sh
+done
 
 echo "echo ERROR COUNTER: \$ERROR_COUNT
 if [ \$ERROR_COUNT -gt 0 ]; then
-  mv $manifest_file $manifest_file.error
   exit 2
-else
-  mv $manifest_file $manifest_file.done
 fi" >> ./run.sh
 
 chmod +x ./run.sh
