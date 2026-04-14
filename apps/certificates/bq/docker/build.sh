ad336bacabdd700ba399dab17c5672b02198a03f
Michelangelo <ms.co.it@gmail.com>
2026-04-14 14:11:48 +0000

    app certs bq dockerfile image name

--- apps/certificates/bq/docker/build.sh
+++ apps/certificates/bq/docker/build.sh
@@ -5,6 +5,6 @@ if [ -z "$TYPE" ] || [ -z "$APPNAME" ]; then
   echo "Error: TYPE or APPNAME is not set."
   exit 1
 fi
-gcloud beta run jobs deploy $TYPE-$APPNAME-job --source=. --execute-now --region=europe-west1 --max-retries=1 \
+gcloud beta run jobs deploy $TYPE-$APPNAME-job --source=. --region=europe-west1 --max-retries=1 \
 --add-volume name=gcs-1,bucket=rws-data,type=cloud-storage \
 --add-volume-mount volume=gcs-1,mount-path=/data
