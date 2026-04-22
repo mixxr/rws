2672786d6831387f661dcda672f4cc7ffa54a5a8
Michelangelo <ms.co.it@gmail.com>
2026-04-22 18:47:01 +0200

    isin_ticker

--- apps/details/bq/docs/create_table.sql
+++ apps/details/bq/docs/create_table.sql
@@ -1,9 +1,10 @@
 DROP TABLE `invcerts.ISINs.isin_ticker`;
 CREATE TABLE `invcerts.ISINs.isin_ticker` (
   certificate_isin STRING NOT NULL,
+  certificate_name STRING NOT NULL,
+  stock_isin STRING NOT NULL,
   stock_name STRING,
   stock_google_finance_ticker STRING,
-  stock_isin STRING NOT NULL,
   stock_exchange STRING,
   stock_sector STRING,
   stock_industry STRING,
