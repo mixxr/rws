7788001d733b32baa57de59e10090c3043aaebff
Michelangelo <ms.co.it@gmail.com>
2026-05-18 10:08:47 +0000

    growth export wf completed

---
+++ apps/bq/export-scripts/growth-wf.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+./growth_export.sh
+./growth_consolidate.sh certs_growth-tmp1.csv
+./replace-not-provided.sh certs_growth-tmp1.csv certs_growth-tmp2.csv
+./remove-dupl-isin.sh certs_growth-tmp2.csv certs_growth-final.csv
+rm certs_growth-tmp*.csv 
+echo "Completed."
