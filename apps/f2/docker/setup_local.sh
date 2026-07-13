45a20a401f201fee080889a136fdf76bba5c4271
Michelangelo <ms.co.it@gmail.com>
2026-07-13 15:43:55 +0000

    minors f2.done added

--- apps/f2/docker/setup_local.sh
+++ apps/f2/docker/setup_local.sh
@@ -2,7 +2,7 @@
 echo "Setup Local running...NOTE: IT IS NEEDED **ONLY** WHEN setup.sh runs locally!!"
 TYPE="quotes"
 BUCKET_URI="gs://rws-data"
-BUCKET_URI_OUTPUT="$BUCKET_URI/$TYPE/ouput"
+BUCKET_URI_OUTPUT="$BUCKET_URI/$TYPE/output"
 BUCKET_URI_JOBS="$BUCKET_URI/$TYPE/jobs/2"
 WORKDIR="."
 WORKDIR_OUTPUT="$WORKDIR/$TYPE/output/*"
@@ -10,7 +10,18 @@ WORKDIR_JOBS="$WORKDIR/$TYPE/jobs/2/*"
 
 ./setup.sh
 
-echo "Uploading $WORKDIR_JOBS to $BUCKET_URI_JOBS..."
-gcloud storage cp --recursive $WORKDIR_JOBS $BUCKET_URI_JOBS/ || true
-echo "Uploading $WORKDIR_OUTPUT to $BUCKET_URI_OUTPUT..."
+for f in "$WORKDIR_JOBS".f2.done; do
+    # Extract base filename without .done
+    base=$(basename "$f" .done)
+
+    # Remote file.f2 to delete
+    remote="$BUCKET_URI_JOBS/$base"
+    echo "Deleting $remote..."
+    gcloud storage rm "$remote" || true
+
+    echo "Uploading $f → $BUCKET_URI_JOBS/$base.done..."
+    gcloud storage cp "$f" "$BUCKET_URI_JOBS/$base.done"
+done
+
+echo "Uploading $WORKDIR_OUTPUT → $BUCKET_URI_OUTPUT..."
 gcloud storage cp --recursive $WORKDIR_OUTPUT $BUCKET_URI_OUTPUT/ || true
