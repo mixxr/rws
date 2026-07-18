e5e45842fae81c460b2e30736d5796c581e8550e
Michelangelo <ms.co.it@gmail.com>
2026-07-18 13:58:29 +0000

    r2 get

--- apps/sb/wf/run_dwl.sh
+++ apps/sb/wf/run_dwl.sh
@@ -2,5 +2,5 @@
 
 cd ./scripts
 echo "Copying rws/ws/tickers_index.csv to ./config/ ..."
-npx wrangler r2 object get rws/ws/sectors/$datetime/ --file ./config/ --remote
+npx wrangler r2 object get rws/ws/tickers_index.csv --file ./config/ --remote
 
