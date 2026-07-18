0682a9ad13818f62b78e32b1c492ed5225cec142
Michelangelo <ms.co.it@gmail.com>
2026-07-18 17:26:03 +0000

    multiple file upl

--- apps/sb/wf/run_upl.sh
+++ apps/sb/wf/run_upl.sh
@@ -3,4 +3,7 @@
 cd ./scripts
 datetime=$(TZ="Europe/Rome" date +"%Y-%m-%dT%H-%M-%S")
 echo "Copying ./_sectors/* to rws/ws/sectors/$datetime/ ..."
-npx wrangler r2 object put rws/ws/sectors/$datetime/ --file ./_sectors/* --remote
+for f in ./_sectors/*; do
+    npx wrangler r2 object put rws/ws/sectors/$datetime/ --file "$f" --remote
+done
+
