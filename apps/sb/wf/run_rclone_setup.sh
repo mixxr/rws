480ad7920588b47e266a7a3e521e788ea1562faf
Michelangelo <ms.co.it@gmail.com>
2026-07-19 08:51:42 +0000

    rclone

---
+++ apps/sb/wf/run_rclone_setup.sh
@@ -0,0 +1,14 @@
+echo "[r2]
+type = s3
+provider = Cloudflare
+access_key_id = d4f4c5e6069f8817b00920366c47c6ef
+secret_access_key = $R2_CLONE_SECRET
+endpoint = https://7bf21e6595296f2bba6b11290649d9b0.r2.cloudflarestorage.com
+acl = private" > ~/.config/rclone/rclone.conf
+ls -la ~/.config/rclone/rclone.conf
+if grep -qF "$R2_CLONE_SECRET" ~/.config/rclone/rclone.conf; then
+  echo "R2_CLONE_SECRET found"
+else
+  echo "R2_CLONE_SECRET not found. Exiting..."
+  exit 1
+fi
--- apps/sb/wf/run_upl.sh
+++ apps/sb/wf/run_upl.sh
@@ -3,8 +3,9 @@
 cd ./scripts
 datetime=$(TZ="Europe/Rome" date +"%Y-%m-%dT%H-%M-%S")
 echo "Copying ./_sectors/* to rws/ws/sectors/$datetime/ ..."
-for f in ./_sectors/*; do
-    filename="$(basename "$f")"
-    npx wrangler r2 object put rws/ws/sectors/$datetime/$filename --file "$f" --remote
-done
+# for f in ./_sectors/*; do
+#     filename="$(basename "$f")"
+#     npx wrangler r2 object put rws/ws/sectors/$datetime/$filename --file "$f" --remote
+# done
+rclone copy ./_sectors/ r2:rws/ws/sectors/$datetime/
 
