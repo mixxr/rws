3ec33d29d833d1c586406a76850df00f2f263658
Michelangelo <ms.co.it@gmail.com>
2026-08-04 17:44:43 +0000

    minors fix 9028934-5

--- apps/in/docker/create_stats.sh
+++ apps/in/docker/create_stats.sh
@@ -4,8 +4,6 @@ echo "Downloading CSV files from GCP and CF to prepare stats"
 echo "Downloading from CF..."
 rclone copy r2:rws/ws ./helpers/
 
-ls -la ./helpers/
-
 echo "Downloading from GCP..."
 cd ./helpers
 source ./utils.sh
@@ -22,7 +20,7 @@ while IFS=";" read -r issuer all; do
 done < <(tail -n +2 "$INPUT_FILE")
 
 echo "Creating stats..."
-ls -la
+
 items=( "details" "issuers" "tickers" "tickers_index")
 echo "type;count" > "stats.csv"
 for item in "${items[@]}"; do
